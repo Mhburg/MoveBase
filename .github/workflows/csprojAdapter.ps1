70ece8cb7d4fd7ef4cf0f60e5eed27a1a497542b
Mhburg <Morgan_lee5@hotmail.com>
2020-08-12 02:41:45 -0400

    Setup release workflow

---
+++ .github/workflows/csprojAdapter.ps1
@@ -0,0 +1,27 @@
+﻿param ([string]$filePath)
+[xml]$xdoc  = get-content $filePath
+
+# Use dll reference instead of project reference
+$reference = $xdoc.Project.ItemGroup.Reference[0].Clone()
+$reference.SetAttribute("Include", "NotooShabby.RimWorldUtility")
+$reference.Private = "False"
+$xdoc.Project.ItemGroup.AppendChild($reference)
+
+# Remove project reference
+$nsmgr = New-Object -TypeName System.Xml.XmlNamespaceManager -ArgumentList $xdoc.NameTable
+$nsmgr.AddNamespace("ns", $xdoc.DocumentElement.NamespaceURI)
+$delete = $xdoc.DocumentElement.SelectSingleNode('/ns:Project/ns:ItemGroup/ns:ProjectReference[ns:Name="RimWorldUtility"]', $nsmgr)
+$delete.ParentNode.RemoveChild($delete)
+
+# Remove generation of documentation file
+$delete = $xdoc.DocumentElement.SelectNodes('/ns:Project/ns:PropertyGroup/ns:DocumentationFile', $nsmgr)
+Foreach ($doc in $delete)
+{
+    $doc.ParentNode.RemoveChild($doc)
+}
+
+# Remove PostBuild event used by MSVS
+$delete = $xdoc.DocumentElement.SelectSingleNode('/ns:Project/ns:PropertyGroup/ns:PostBuildEvent', $nsmgr)
+$delete.ParentNode.RemoveChild($delete)
+
+$xdoc.Save($filePath)
---
+++ .github/workflows/packMod.ps1
@@ -0,0 +1,38 @@
+﻿param ([string]$ModDir, [string]$Src)
+
+$common = "Common"
+$about = "About"
+$loadFolder = "LoadFolders.xml"
+$defs = "Defs"
+$languages = "Languages"
+$patches = "Patches"
+$texture = "Textures"
+$assemblies = "Assemblies"
+$RwUtil = "NotooShabby.RimWorldUtility.dll"
+
+# Copy About folder
+Copy-Item -Path ([System.IO.Path]::Combine($Src, $about)) -Destination ([System.IO.Path]::Combine($ModDir, $about)) -Recurse -PassThru
+
+# Copy LoadFolders.xml
+Copy-Item ([System.IO.Path]::Combine($Src, $loadFolder)) -Destination $ModDir -PassThru
+
+# Copy supporting libraries
+$destination = ([System.IO.Path]::Combine($ModDir, $common, $assemblies, $RwUtil))
+New-Item -ItemType File -Path $destination -Force
+Copy-Item "$Src\..\references\$RwUtil" -Destination $destination -Force -PassThru
+
+# Copy defs folder
+Copy-Item -Path ([System.IO.Path]::Combine($Src, $defs)) -Destination ([System.IO.Path]::Combine($ModDir, $common, $defs)) -Recurse -PassThru
+
+# Copy Languages folder
+Copy-Item -Path ([System.IO.Path]::Combine($Src, $languages)) -Destination ([System.IO.Path]::Combine($ModDir, $common, $languages)) -Recurse -PassThru
+
+# Copy Patches folder
+Copy-Item -Path ([System.IO.Path]::Combine($Src, $patches)) -Destination ([System.IO.Path]::Combine($ModDir, $common, $patches)) -Recurse -PassThru
+
+# Copy Textures folder
+Copy-Item -Path ([System.IO.Path]::Combine($Src, $texture)) -Destination ([System.IO.Path]::Combine($ModDir, $common, $texture)) -Recurse -PassThru
+
+# Zip the directory
+$dir = New-Object -TypeName System.IO.DirectoryInfo -ArgumentList $ModDir
+Compress-Archive -Path "$ModDir\*" -DestinationPath ".\$($dir.Name).zip"
